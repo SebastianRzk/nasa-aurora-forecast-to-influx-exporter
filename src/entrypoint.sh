2e091b8484c02caf80e59dae9b2cbb9c29d8978c
SebastianRzk <github@sebastianruziczka.de>
2024-05-20 14:43:20 +0200

    fix venv path

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,7 +6,7 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-0 */4  * * * source venv/bin/activate && python3 /src/main.py
+0 */4  * * * source /venv/bin/activate && python3 /src/main.py
 # This extra line makes it a valid cron" > scheduler.txt;
 
 source venv/bin/activate && python3 /src/main.py
